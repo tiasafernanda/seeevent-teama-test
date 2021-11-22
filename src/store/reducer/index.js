d77e7b5c24f4bd0f7bb5187ed552454d33619e15
ardhyleo <adr_leo94@yahoo.com>
2021-11-22 23:05:33 +0700

    feat: fixing all of it par 3

--- src/store/reducer/index.js
+++ src/store/reducer/index.js
@@ -1,10 +1,5 @@
-<<<<<<< HEAD
 import { combineReducers } from "redux";
 import event from "./event";
-=======
-import { combineReducers } from 'redux';
-import event from './event';
->>>>>>> 589b82885d41e34552190b4e96b63bde397ccfe7
 const rootReducers = combineReducers({
   event,
 });
--- src/store/sagas/index.js
+++ src/store/sagas/index.js
@@ -1,19 +1,8 @@
-<<<<<<< HEAD
 import { all } from "redux-saga/effects";
 import eventSaga from "./eventSaga";
 import { watchLogin } from "./auth";
-import { watchGetEvents, watchGetEventDetail } from "./event";
+import { watchGetEvents, watchGetEventDetail, watchEventSearch } from "./event";
 
 export default function* rootSaga() {
-  yield all([watchGetEvents(), watchGetEventDetail(), watchLogin(), eventSaga()]);
-=======
-import { all } from 'redux-saga/effects';
-import eventSaga from './eventSaga';
-import { watchLogin } from './auth';
-import { watchGetEvents, watchEventSearch } from './event';
-// import { watchEventSearch } from './event';
-
-export default function* rootSaga() {
-  yield all([eventSaga(), watchGetEvents(), watchLogin(), watchEventSearch()]);
->>>>>>> 589b82885d41e34552190b4e96b63bde397ccfe7
+  yield all([watchGetEvents(), watchGetEventDetail(), watchLogin(), eventSaga(), watchEventSearch()]);
 }
