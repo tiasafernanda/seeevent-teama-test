32edcf207bef6bc2edc79bf2abc58edeb9899a3d
tiasafernanda <tiasafernanda@gmail.com>
2021-11-22 23:39:51 +0700

    fixing

--- src/account/Accountpage.jsx
+++ src/account/Accountpage.jsx
@@ -1,8 +1,8 @@
-import React from "react";
-import "./AccountStyle.css";
-import edit3 from "./Assets/edit-3.png";
-import Left from "./Assets/Left.png";
-import { useEffect } from "react";
+import React from 'react';
+import './AccountStyle.css';
+import edit3 from './Assets/edit-3.png';
+import Left from './Assets/Left.png';
+import { useEffect } from 'react';
 // import header from "./Assets/header.png";
 // import { formatProfileName } from "../helper/FormatProfileName.js";
 
@@ -10,28 +10,32 @@ const AccountPage = () => {
   // const first_name = storeGlobal && storeGlobal.data.data.user.first_name;
   // const last_name = storeGlobal && storeGlobal.data.data.user.last_name;
   const errorImage = true;
+  const logout = () => {
+    localStorage.removeItem('token');
+    window.location.href = '/';
+  };
   return (
     <>
       {/* //account */}
-      <div className="Container1">
-        <div className="account-container">
-          <div className="profile">
+      <div className='Container1'>
+        <div className='account-container'>
+          <div className='profile'>
             {/* {errorImage ? <div className="picture-account">{dataEvent && formatProfileName(first_name, last_name)}</div> : <img src={header} alt="profile picture" className="picture-account" />} */}
-            <div className="profile-container">
+            <div className='profile-container'>
               <h4>This is Profile</h4>
               <p>ini alamat email</p>
             </div>
           </div>
         </div>
         {/* //button */}
-        <div className="btn-container">
-          <button className="edit-btn">
-            <img src={edit3} className="edit" />
-            <div className="edit">Edit Profile</div>
+        <div className='btn-container'>
+          <button className='edit-btn'>
+            <img src={edit3} className='edit' />
+            <div className='edit'>Edit Profile</div>
           </button>
-          <button className="signout-btn" onClick={Left}>
+          <button className='signout-btn' onClick={logout}>
             <img src={Left} />
-            <div className="signout">Sign out</div>
+            <div className='signout'>Sign out</div>
           </button>
         </div>
       </div>
--- src/routes/Router.js
+++ src/routes/Router.js
@@ -1,25 +1,25 @@
-import React from "react";
-import { Route, Routes } from "react-router-dom";
-import HomeLogin from "../pages/home-login/HomeLogin";
-import SuccessLogin from "../pages/home-succes-login/SuccessLogin";
-import LoginForm from "../pages/Login-Form/LoginForm";
-import SignUpForm from "../pages/Sign-Up-Form/SignUpForm";
-import SearchEvent from "../pages/searchEvent/SearchEvent";
-import DetailPage from "../comment/DetailPage";
-import AccountPage from "../account/Accountpage";
+import React from 'react';
+import { Route, Routes } from 'react-router-dom';
+import HomeLogin from '../pages/home-login/HomeLogin';
+import SuccessLogin from '../pages/home-succes-login/SuccessLogin';
+import LoginForm from '../pages/Login-Form/LoginForm';
+import SignUpForm from '../pages/Sign-Up-Form/SignUpForm';
+import SearchEvent from '../pages/searchEvent/SearchEvent';
+import DetailPage from '../comment/DetailPage';
+import AccountPage from '../account/Accountpage';
 
 export default function Router() {
   return (
     <div>
       <Routes>
-        <Route path="/" exact element={<HomeLogin />} />
-        <Route path="/success" exact element={<SuccessLogin />} />
-        <Route path="/login" exact element={<LoginForm />} />
-        <Route path="/signup" exact element={<SignUpForm />} />
-        <Route path="/search/:keyword" exact element={<SearchEvent />} />
-        <Route path="/account-page" element={<AccountPage />} />
-        <Route path="/comment-page" element={<CommentPage />} />
-        <Route path="/detail-page/:id" element={<DetailPage />} />
+        <Route path='/' exact element={<HomeLogin />} />
+        <Route path='/success' exact element={<SuccessLogin />} />
+        <Route path='/login' exact element={<LoginForm />} />
+        <Route path='/signup' exact element={<SignUpForm />} />
+        <Route path='/search/:keyword' exact element={<SearchEvent />} />
+        <Route path='/account-page' element={<AccountPage />} />
+        {/* <Route path="/comment-page" element={<CommentPage />} /> */}
+        <Route path='/detail-page/:id' element={<DetailPage />} />
       </Routes>
     </div>
   );
